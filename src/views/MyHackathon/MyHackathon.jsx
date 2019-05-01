d82579ab72ae8f93bb2e9320a5eeb15013d67b25
Yadav <priyalagrawal0810@gmail.com>
2019-05-01 11:08:06 -0700

    Integrated Get myhackathon API

--- src/views/MyHackathon/MyHackathon.jsx
+++ src/views/MyHackathon/MyHackathon.jsx
@@ -25,25 +25,38 @@ import loginPageStyle from "assets/jss/material-kit-react/views/loginPage.jsx";
 
 import image from "assets/img/bg7.jpg";
 
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from "react-google-login";
 // @material-ui/icons
 import Group from "@material-ui/icons/Group";
 import Assessment from "@material-ui/icons/Assessment";
 
 // core components
 import InfoArea from "components/InfoArea/InfoArea.jsx";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+import axios from "axios";
 class UserOrganization extends React.Component {
   constructor(props) {
     super(props);
     // we use this to make the card to appear after the page has been rendered
     this.state = {
-      cardAnimaton: "cardHidden"
+      cardAnimaton: "cardHidden",
+      hackathon: {
+        hackathonId: null,
+        hackathonName: null,
+        teamId: null,
+        teamName: null,
+        participants: [],
+        paymentDone: null,
+        score: 0,
+        submissionURL: null,
+        teamLeadId: null
+      }
     };
   }
   componentDidMount() {
@@ -54,35 +67,56 @@ class UserOrganization extends React.Component {
       }.bind(this),
       700
     );
+    this.getMyHackathons();
   }
-  
-  
+
+  getMyHackathons() {
+    axios
+      .get("http://localhost:5000/participant/1/hackathon/1")
+      .then(response => {
+        console.log(response);
+        var hackathon = {};
+        hackathon.hackathonId = response.data.hackathonId;
+        hackathon.hackathonName = response.data.hackathonName;
+        hackathon.teamId = response.data.teamId;
+        hackathon.teamName = response.data.teamName;
+        hackathon.participants = response.data.participants;
+        // for (var i = 0; i < response.data.participants; i++) {
+        //   hackathon.participants.push(response.data.participants[i]);
+        // }
+        hackathon.paymentDone = response.data.paymentDone;
+        hackathon.score = response.data.score;
+        hackathon.submissionURL = response.data.submissionURL;
+        hackathon.teamLeadId = response.data.teamLeadId;
+        this.setState({ hackathon: hackathon });
+      });
+  }
   render() {
     const styles = theme => ({
       root: {
-        width: '100%',
+        width: "100%",
         marginTop: theme.spacing.unit * 3,
-        overflowX: 'auto',
+        overflowX: "auto"
       },
       table: {
-        minWidth: 700,
-      },
+        minWidth: 700
+      }
     });
-    
+
     let id = 0;
     function createData(name, role, payment_status, amount) {
       id += 1;
       return { id, name, role, payment_status, amount };
     }
-    
-    const rows = [
-      createData('A', 'BackEnd Engineer', 'Done', '$240'),
-      createData('B', 'FE Engineer', 'Done', '$240'),
-      createData('C', 'DB Engineer', 'Pending', '$240'),
-      createData('D', 'Designer', 'Done', '$240'),
+
+    var rows = [
+      createData("A", "BackEnd Engineer", "Done", "$240"),
+      createData("B", "FE Engineer", "Done", "$240"),
+      createData("C", "DB Engineer", "Pending", "$240"),
+      createData("D", "Designer", "Done", "$240")
     ];
-    
-    const responseGoogle = (response) => {
+
+    const responseGoogle = response => {
       // var profile = response.getBasicProfile();
       // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
       // console.log('Full Name: ' + profile.getName());
@@ -95,7 +129,7 @@ class UserOrganization extends React.Component {
       // var id_token = response.getAuthResponse().id_token;
       // console.log("ID Token: " + id_token);
       console.log(response);
-    }
+    };
     const { classes, ...rest } = this.props;
     return (
       <div>
@@ -108,27 +142,25 @@ class UserOrganization extends React.Component {
           }}
         >
           <div className={classes.container}>
-          
-            <GridContainer style={{backgroundColor: "white"}}>
-            
-             <GridItem  xs={4} sm={2} md={3}>
-                  <InfoArea 
-                    title="Hackathon Name"
-                    description="Hackathon 2"
-                    icon={Code}
-                    iconColor="rose"
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}></GridItem>
-                <GridItem  xs={12} sm={12} md={2}>
-                  <InfoArea
-                    title="Score"
-                    description="250"
-                    icon={Score}
-                    iconColor="rose"
-                  />
-                </GridItem>
-                {/* <GridItem  xs={12} sm={12} md={2}>
+            <GridContainer style={{ backgroundColor: "white" }}>
+              <GridItem xs={4} sm={2} md={3}>
+                <InfoArea
+                  title={this.state.hackathon.hackathonName}
+                  description="Not getting from backend"
+                  icon={Code}
+                  iconColor="rose"
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4} />
+              <GridItem xs={12} sm={12} md={2}>
+                <InfoArea
+                  title="Score"
+                  description={this.state.hackathon.score}
+                  icon={Score}
+                  iconColor="rose"
+                />
+              </GridItem>
+              {/* <GridItem  xs={12} sm={12} md={2}>
                   <InfoArea
                     title="Status"
                     description="Pending"
@@ -136,28 +168,28 @@ class UserOrganization extends React.Component {
                     iconColor="rose"
                   />
                 </GridItem> */}
-                {/* <GridItem  xs={12} sm={12} md={4}>
+              {/* <GridItem  xs={12} sm={12} md={4}>
                 <InfoArea
                   title="Status"
                   description="Pending"
                   icon={Assessment}
                   iconColor="rose"
                 /></GridItem> */}
-                
-                <GridItem  xs={12} sm={12} md={12}>
-                  <hr/>
-                </GridItem>
-                {/* <GridItem >
+
+              <GridItem xs={12} sm={12} md={12}>
+                <hr />
+              </GridItem>
+              {/* <GridItem >
                   <input type="search" 
                 style={{marginTop: 10,  width: 500, height: 50}}
                 ></input>
                 </GridItem> */}
-                <GridItem  xs={12} sm={12} md={12}>
-                <h2 style={{color: "black"}}>Team</h2>
-                </GridItem>
-                <GridItem  xs={12} sm={12} md={12}>
+              <GridItem xs={12} sm={12} md={12}>
+                <h2 style={{ color: "black" }}>Team</h2>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={12}>
                 <Paper className={classes.root}>
-                  <Table className={classes.table}  style={{marginBottom: 30}}>
+                  <Table className={classes.table} style={{ marginBottom: 30 }}>
                     <TableHead>
                       <TableRow>
                         <TableCell>Name</TableCell>
@@ -167,20 +199,22 @@ class UserOrganization extends React.Component {
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {rows.map(row => (
-                        <TableRow key={row.id}>
+                      {this.state.hackathon.participants.map(row => (
+                        <TableRow key={row.userId}>
                           <TableCell component="th" scope="row">
                             {row.name}
                           </TableCell>
-                          <TableCell align="left">{row.role}</TableCell>
-                          <TableCell align="left">{row.payment_status}</TableCell>
-                          <TableCell align="left">{row.amount}</TableCell>
+                          <TableCell align="left">{row.title}</TableCell>
+                          <TableCell align="left">
+                            {row.paymentDone ? "Done" : "Pending"}
+                          </TableCell>
+                          <TableCell align="left">{0}</TableCell>
                         </TableRow>
                       ))}
                     </TableBody>
                   </Table>
                 </Paper>
-                </GridItem>
+              </GridItem>
             </GridContainer>
           </div>
         </div>
