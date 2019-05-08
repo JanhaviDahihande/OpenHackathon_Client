47ba1663eb8808ba552fba77b1fca5c36300a2ad
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-05-08 01:41:23 -0700

    Update ProfilePage.jsx

--- src/views/ProfilePage/ProfilePage.jsx
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -16,6 +16,8 @@ import GridItem from "components/Grid/GridItem.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import NavPills from "components/NavPills/NavPills.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
+import Card from "components/Card/Card.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
 
 import profile from "assets/img/faces/christian.jpg";
 
@@ -32,7 +34,41 @@ import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
 import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
 
+import CardBody from "components/Card/CardBody.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import { Link } from "react-router-dom";
+
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import People from "@material-ui/icons/People";
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+
 class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+    // we use this to make the card to appear after the page has been rendered
+    this.state = {
+      editable : 1
+    };
+    this.editProfile = this.editProfile.bind(this);
+    this.updateProfile = this.updateProfile.bind(this);
+  }
+
+  editProfile() {
+    this.setState({editable: 1})
+  }
+
+  updateProfile() {
+    this.setState({editable: 0})
+  }
+
   render() {
     const { classes, ...rest } = this.props;
     const imageClasses = classNames(
@@ -41,6 +77,250 @@ class ProfilePage extends React.Component {
       classes.imgFluid
     );
     const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+    var comp =
+      this.state.editable == 1 ? (
+        <div className={classes.container} style={{ paddingTop: 100 }}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8}>
+                <Card className={classes[this.state.cardAnimaton]}>
+                  <form className={classes.form}>
+                    <CardBody>
+                      <CustomInput
+                        labelText="First Name"
+                        id="first_name"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Last Name"
+                        id="last_name"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Screen name"
+                        id="screen_name"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "email"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Street"
+                        id="street"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="City"
+                        id="city"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="State"
+                        id="state"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Zip"
+                        id="zip"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Title"
+                        id="title"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Organization"
+                        id="organization"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="Portrait URL"
+                        id="portrait_url"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                       <CustomInput
+                        labelText="About me"
+                        id="about_me"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          onChange: this.handleChange,
+                          type: "text"
+                        }}
+                      />
+                      <br />
+                    </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button
+                        simple
+                        color="primary"
+                        size="lg"
+                        onClick={this.updateProfile}
+                      >
+                        Update
+                      </Button>
+                    </CardFooter>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+        //section 1 ends here
+      ) : this.state.editable == 0 ? (
+        <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <Card className={classes[this.state.cardAnimaton]}>
+            <form className={classes.form}>
+              <CardBody>
+              <List className={classes.root}>
+                <ListItem>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                  <ListItemText primary="Name" secondary="Janhavi" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <WorkIcon />
+                  </Avatar>
+                  <ListItemText primary="Last name" secondary="Dahihande" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                  <ListItemText primary="Screen name" secondary="Screen name" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                  <ListItemText primary="Email" secondary="janhavi.dahihande@sjsu.edu" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <WorkIcon />
+                  </Avatar>
+                  <ListItemText primary="City" secondary="Pune" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                  <ListItemText primary="State" secondary="Maharashtra" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                  <ListItemText primary="Title" secondary="Title" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <WorkIcon />
+                  </Avatar>
+                  <ListItemText primary="Organization" secondary="San Jose State University" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                  <ListItemText primary="Portrait URL" secondary="http://portraiturl.com" />
+                </ListItem>
+                <ListItem>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                  <ListItemText primary="About Me" secondary="Here I am! This is me!" />
+                </ListItem>
+              </List>
+              </CardBody>
+              <CardFooter className={classes.cardFooter}>
+                      <Button
+                        simple
+                        color="primary"
+                        size="lg"
+                        onClick={this.editProfile}
+                      >
+                        Edit
+                      </Button>
+                    </CardFooter>
+                    </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+      ) : (
+        ""
+      );
     return (
       <div>
         <Header
@@ -56,8 +336,18 @@ class ProfilePage extends React.Component {
         />
         <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
         <div className={classNames(classes.main, classes.mainRaised)}>
-          <div>
-            <div className={classes.container}>
+          <div className={classes.container}>
+          <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={6}>
+                  <div className={classes.profile}>
+                    <div>
+                      <img src={profile} alt="..." className={imageClasses} />
+                    </div>
+                  </div>
+                </GridItem>
+              </GridContainer>
+          {comp}
+ {/* <div className={classes.container}>
               <GridContainer justify="center">
                 <GridItem xs={12} sm={12} md={6}>
                   <div className={classes.profile}>
@@ -204,7 +494,7 @@ class ProfilePage extends React.Component {
                   />
                 </GridItem>
               </GridContainer>
-            </div>
+            </div> */}
           </div>
         </div>
         <Footer />
